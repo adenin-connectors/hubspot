7e8298c7cd26ec516759c73c79ea0723243152a3
Danilo Markovic <dmarkovic@smartnet.rs>
2019-05-08 10:21:51 +0200

    Merged ticket-status and ticket-recent into ticket activity.

--- activities/mytickets.js
+++ activities/mytickets.js
@@ -33,11 +33,24 @@ module.exports = async function (activity) {
 
     let tickets = api.filterOpenTickets(response.body.objects);
     tickets = api.filterMyTickets(currentOwnerId, tickets);
+    const dateRange = $.dateRange(activity, "today");
+    tickets = api.filterTicketsByDateRange(tickets, dateRange);
 
     activity.Response.Data.items = api.mapTicketsToItems(tickets);
+    let value = activity.Response.Data.items.items.length;
     activity.Response.Data.title = T(activity, 'Open Tickets');
     activity.Response.Data.link = `https://app.hubspot.com/contacts/${currentUser.body.portalId}/tickets/list/view/all/`;
     activity.Response.Data.linkLabel = T(activity, 'All Tickets');
+    activity.Response.Data.actionable = value > 0;
+
+    if (value > 0) {
+      activity.Response.Data.value = value;
+      activity.Response.Data.color = 'blue';
+      activity.Response.Data.description = value > 1 ? T(activity, "You have {0} tickets.", value)
+        : T(activity, "You have 1 ticket.");
+    } else {
+      activity.Response.Data.description = T(activity, `You have no tickets.`);
+    }
 
     if (response.body.hasMore) {
       activity.Response.Data._nextpage = response.body.offset;
--- activities/tickets.js
+++ activities/tickets.js
@@ -15,13 +15,26 @@ module.exports = async function (activity) {
     const response = await api(url);
     if ($.isErrorResponse(activity, response)) return;
 
-    const openTickets = api.filterOpenTickets(response.body.objects);
+    let tickets = api.filterOpenTickets(response.body.objects);
+    const dateRange = $.dateRange(activity, "today");
+    tickets = api.filterTicketsByDateRange(tickets, dateRange);
 
-    activity.Response.Data.items = api.mapTicketsToItems(openTickets);
+    activity.Response.Data.items = api.mapTicketsToItems(tickets);
+    let value = activity.Response.Data.items.items.length;
     activity.Response.Data.title = T(activity, 'Open Tickets');
     activity.Response.Data.link = `https://app.hubspot.com/contacts/${currentUser.body.portalId}/tickets/list/view/all/`;
     activity.Response.Data.linkLabel = T(activity, 'All Tickets');
-    
+    activity.Response.Data.actionable = value > 0;
+
+    if (value > 0) {
+      activity.Response.Data.value = value;
+      activity.Response.Data.color = 'blue';
+      activity.Response.Data.description = value > 1 ? T(activity, "You have {0} tickets.", value)
+        : T(activity, "You have 1 ticket.");
+    } else {
+      activity.Response.Data.description = T(activity, `You have no tickets.`);
+    }
+
     if (response.body.hasMore) {
       activity.Response.Data._nextpage = response.body.offset;
     }
